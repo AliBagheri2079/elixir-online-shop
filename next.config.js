4733720ddb1a141a8390ba6c97279e67f39bcb9d
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-23 20:24:22 +0330

    🧹 chore(app): update next config
    update next config with optimize mantine packages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
