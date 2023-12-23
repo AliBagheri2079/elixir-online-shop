e27d61d939e11b76385e7ec918464055204eaa4d
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-24 00:34:28 +0330

    🧹 chore(app): update next configuration
    update next configuration with adding swell images pattern

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.swell.store',
+        port: '',
+        pathname: '/elixir-online-shop_test/**',
+      },
+    ],
+  },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
