52605d96a341074e8716efc003cd397b3db73afc
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-23 20:02:13 +0330

    ✨ feat(app): create swell change locale helper
    create swell change locale utility helper

---
+++ src/utils/helpers/change-swell-locale.ts
@@ -0,0 +1,8 @@
+import { swellJS } from '@/config';
+import type { Locale } from '@/types';
+
+export function changeSwellLocale(lang: Locale) {
+  const defaultLocale = 'en';
+  const alternateLocale = 'az';
+  return swellJS.locale.select(lang === 'en' ? defaultLocale : alternateLocale);
+}
